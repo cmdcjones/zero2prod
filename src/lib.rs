78ee0435a456ead81c8453b47761b9b6acaa6a43
Dominique Chin-Jones <djones@rapptrlabs.com>
2024-11-26 09:21:29 -0500

    picking back up from before

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(health_check)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run();
+
+    Ok(server)
 }
