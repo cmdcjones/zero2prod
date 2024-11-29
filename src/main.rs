590f67f7a59d9f5cf684ca64af305e7300de2e3b
Dominique Chin-Jones <djones@rapptrlabs.com>
2024-11-29 12:25:07 -0500

    implement tracing and logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::net::TcpListener;
 
 use sqlx::Pool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to initialize logger");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set global subscriber");
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = Pool::connect(&configuration.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use actix_web::web;
 use actix_web::HttpResponse;
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,6 +13,15 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Saving new subscriber",
+        %request_id,
+        subscriber_name = %form.name,
+        subscriber_email = %form.email,
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving subscriber details");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,11 +33,12 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
         Utc::now()
     )
     .execute(db_pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => HttpResponse::Created().finish(),
         Err(e) => {
-            println!("Failed to execute query {}", e);
+            tracing::error!("RID {} Failed to execute query {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
